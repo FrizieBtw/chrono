8be78245940246dd9acfefcc1766e408ff1c4ad7
Paulo Amaral <frizie08@gmail.com>
2023-10-06 19:01:58 +0200

    chronomètre fonctionnel

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+play = document.getElementById("play");
 reinit = document.getElementById("reinit");
+pause = document.getElementById("pause");
 h = document.getElementById("h");
 m = document.getElementById("m");
 s = document.getElementById("s");
@@ -11,6 +13,55 @@ subS = document.getElementById("subS");
 let heure = 0;
 let minute = 1;
 let seconde = 0;
+let interval = null;
+
+function addHeure() {
+    heure += 1;
+    if (heure == 100) {
+        heure = 0;
+    }
+    reecrireTemps(h, heure)
+}
+
+function addMinute() {
+    minute += 1;
+    if (minute == 60) {
+        minute = 0;
+    }
+    reecrireTemps(m, minute)
+}
+
+function addSeconde() {
+    seconde += 1;
+    if (seconde == 60) {
+        seconde = 0;
+    }
+    reecrireTemps(s, seconde)
+}
+
+function subHeure() {
+    heure -= 1;
+    if (heure == -1) {
+        heure = 99;
+    }
+    reecrireTemps(h, heure)
+}
+
+function subMinute() {
+    minute -= 1;
+    if (minute == -1) {
+        minute = 59;
+    }
+    reecrireTemps(m, minute)
+}
+
+function subSeconde() {
+    seconde -= 1;
+    if (seconde == -1) {
+        seconde = 59;
+    }
+    reecrireTemps(s, seconde)
+}
 
 reinit.addEventListener('click', () => {
     heure = 0;
@@ -21,42 +72,67 @@ reinit.addEventListener('click', () => {
     reecrireTemps(s, seconde)
 });
 
-function addHeure() {
-    heure += 1;
-    if (heure == 100) {
-        heure = 0;
+play.addEventListener('click', () => {
+    if (heure != 0 || minute != 0 || seconde != 0) {
+        play.style.display = "none";
+        reinit.style.display = "none";
+        pause.style.display = "flex";
+        interval = setInterval(() => {
+            if (seconde > 0) {
+                seconde -= 1;
+            } else {
+                if (minute > 0) {
+                    minute -= 1;
+                    seconde = 59;
+                } else {
+                    if (heure > 0) {
+                        heure -= 1;
+                        minute = 59;
+                        seconde = 59;
+                    } else {
+                        clearInterval(interval);
+                        play.style.display = "flex";
+                        reinit.style.display = "flex";
+                        pause.style.display = "none";
+                    }
+                }
+            }
+            reecrireTemps(h, heure)
+            reecrireTemps(m, minute)
+            reecrireTemps(s, seconde)
+        }, 1000);
     }
-    reecrireTemps(h, heure)
-}
+});
+
+pause.addEventListener('click', () => {
+    play.style.display = "flex";
+    reinit.style.display = "flex";
+    pause.style.display = "none";
+    clearInterval(interval);
+});
 
 addH.addEventListener('click', () => {
     addHeure();
 });
 
-function addMinute() {
-    minute += 1;
-    if (minute == 60) {
-        minute = 0;
-        addHeure();
-    }
-    reecrireTemps(m, minute)
-}
-
 addM.addEventListener('click', () => {
     addMinute();
 });
 
-function addSeconde() {
-    seconde += 1;
-    if (seconde == 60) {
-        seconde = 0;
-        addMinute();
-    }
-    reecrireTemps(s, seconde)
-}
-
 addS.addEventListener('click', () => {
     addSeconde();
+});
+
+subH.addEventListener('click', () => {
+    subHeure();
+});
+
+subM.addEventListener('click', () => {
+    subMinute();
+});
+
+subS.addEventListener('click', () => {
+    subSeconde();
 });
 
 function reecrireTemps(element, nvT) {
