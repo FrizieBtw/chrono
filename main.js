d86ec6387911685c9f7a7d3db454b11e5ec00b03
Paulo Amaral <pamaralsarai@etu.u-bordeaux.fr>
2023-10-06 13:58:31 +0200

    debut js

---
+++ main.js
@@ -0,0 +1,10 @@
+reinit = document.querySelector("#reinit")
+h = document.querySelector("#h")
+m = document.querySelector("#m")
+s = document.querySelector("#s")
+
+reinit.addEventListener('click', event =>   {
+    h.innerText("00")
+    m.innerText("00")
+    s.innerText("00")
+})
